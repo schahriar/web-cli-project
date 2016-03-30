cc0eb374de82859a27ed4a0cb2269935b93ff31c
Schahriar SaffarShargh <info@schahriar.com>
2016-03-29 20:25:42 -0700

    Abstracted access_token fetching to allow for extensions

--- server.js
+++ server.js
@@ -4,34 +4,50 @@ const express = require("express");
 const app = express();
 const request = require('request');
 
-module.exports = function SERVER(port) {
-  const PORT = port || 8080;
-  
-  app.get('/get_token', function (req, res, next) {
-    if (!req.query.code) return res.json({ error: "OAuth2 Code was not found." });
-
-    request({
+// Service options
+const kServices = {
+  "nest": {
       method: "POST",
       url: "https://api.home.nest.com/oauth2/access_token",
       form: {
-        code: req.query.code,
         client_id: "3b0cea1d-8ff8-405f-a673-57e8e41a4d7d",
         client_secret: process.env.NESTSECRET,
         grant_type: "authorization_code"
       }
-    }, function (error, response, body) {
+    }
+};
+
+module.exports = function SERVER(port) {
+  const PORT = port || 8080;
+  
+  // GET Route for converting OAuth2 codes to access_tokens
+  app.get('/get_token/:service', function (req, res, next) {
+    // Verify Code & Selected Service
+    if (!req.query.code) return res.json({ error: "OAuth2 Code was not found." });
+    if (!req.params.service || (!kServices[req.params.service])) return res.json({ error: "Invalid service." });
+    
+    // Set options
+    let options = kServices[req.params.service];
+    // Assign code
+    options.form.code = req.query.code;
+
+    request(options, function (error, response, body) {
       if (error) return res.json({ error: error });
       
+      // Parse request
       try {
         let result = JSON.parse(body);
+        // If Nest has returned an error log it
         if (result.error) {
           res.json({
             error: result.error_description
           });
         } else {
+          // Log access_token
           res.json(result);
         }
       } catch (e) {
+        // Parsing error
         return res.json({ error: error });
       }
     });
